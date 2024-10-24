2f2c27f98acb0f8416fa961df088c4d1fa462a24
Herbert Kavuma <you@example.com>
2024-10-24 22:06:27 +0300

    chore: refactor

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,18 +1,24 @@
-const path = require('path');
-const { getDefaultConfig } = require('@expo/metro-config');
-const { getConfig } = require('react-native-builder-bob/metro-config');
-const pkg = require('../package.json');
+// const path = require('path');
+// const { getDefaultConfig } = require('@expo/metro-config');
+// const { getConfig } = require('react-native-builder-bob/metro-config');
+// const pkg = require('../package.json');
 
-const root = path.resolve(__dirname, '..');
+// const root = path.resolve(__dirname, '..');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-module.exports = getConfig(getDefaultConfig(__dirname), {
-  root,
-  pkg,
-  project: __dirname,
-});
+// /**
+//  * Metro configuration
+//  * https://facebook.github.io/metro/docs/configuration
+//  *
+//  * @type {import('metro-config').MetroConfig}
+//  */
+// module.exports = getConfig(getDefaultConfig(__dirname), {
+//   root,
+//   pkg,
+//   project: __dirname,
+// });
+
+const { getDefaultConfig } = require('expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = defaultConfig;
